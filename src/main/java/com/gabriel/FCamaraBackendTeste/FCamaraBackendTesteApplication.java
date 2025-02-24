c1d1e2f1c6567901ce0a4ec4efff0faabdc2fac6
gabriel-henri <gabrielhra00@gmail.com>
2025-02-24 10:42:13 -0300

    Commite inicial

---
+++ src/main/java/com/gabriel/FCamaraBackendTeste/FCamaraBackendTesteApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.FCamaraBackendTeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FCamaraBackendTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FCamaraBackendTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/FCamaraBackendTeste/FCamaraBackendTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.FCamaraBackendTeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FCamaraBackendTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
